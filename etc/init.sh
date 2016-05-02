273d53cdd60dc2088793ae2ea562abe35044361a
anatol-s <a.samokisha@gmail.com>
2016-05-02 20:11:28 +0300

    hello app

--- etc/init.sh
+++ etc/init.sh
@@ -8,3 +8,7 @@ fi
 # Nginx
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+
+# Gunicorn
+sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
+sudo /etc/init.d/gunicorn restart
